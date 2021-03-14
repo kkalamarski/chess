0adc6d664fe0a2da88b62a0e75777625347107e9
Krzysztof Kalamarski <k.kalamarski@exlabs.pl>
2021-03-14 00:54:40 -0300

    💄 Make it prettier & fix king AI

--- engine/evaluation/calculatePiecePositionBonus.ts
+++ engine/evaluation/calculatePiecePositionBonus.ts
@@ -1,50 +1,50 @@
-import mapArrayToChessboard from "../../common/mapArrayToChessboard";
-import pieceSquareTables from "./pieceSquareTables";
+import mapArrayToChessboard from '../../common/mapArrayToChessboard'
+import pieceSquareTables from './pieceSquareTables'
 
-const isWhitePiece = (piece: string) => /[A-Z]/.test(piece);
+const isWhitePiece = (piece: string) => /[A-Z]/.test(piece)
 
 const isEndgame = (pieces: { [x: string]: string }) => {
-  const remainingPieces = Object.keys(pieces);
+  const remainingPieces = Object.values(pieces)
 
-  const noQueens = !(
-    remainingPieces.includes("q") || remainingPieces.includes("Q")
-  );
+  const noQueens =
+    !remainingPieces.includes('q') && !remainingPieces.includes('Q')
 
   const maxOneMinorWhite =
-    remainingPieces.filter((piece) => ["N", "B", "R"].includes(piece)).length <=
-    1;
-  const maxOneMinorBlack =
-    remainingPieces.filter((piece) => ["n", "b", "r"].includes(piece)).length <=
-    1;
+    remainingPieces.filter((piece) => ['N', 'B', 'R'].includes(piece)).length <=
+    1
 
-  return noQueens || maxOneMinorWhite || maxOneMinorBlack;
-};
+  const maxOneMinorBlack =
+    remainingPieces.filter((piece) => ['n', 'b', 'r'].includes(piece)).length <=
+    1
+
+  return noQueens || maxOneMinorWhite || maxOneMinorBlack
+}
 
 const calculatePiecePositionBonus = (pieces: { [x: string]: string }): number =>
   Object.entries<string>(pieces)
     .map(([pos, piece]): number => {
-      const isWhite = isWhitePiece(piece);
-      const lowerPiece = piece.toLowerCase();
+      const isWhite = isWhitePiece(piece)
+      const lowerPiece = piece.toLowerCase()
 
-      let squareTable = pieceSquareTables?.[lowerPiece];
+      let squareTable = pieceSquareTables?.[lowerPiece]
 
-      const endgame = isEndgame(pieces);
+      const endgame = isEndgame(pieces)
 
-      if (endgame && lowerPiece === "k") {
-        squareTable = pieceSquareTables?.ke;
+      if (endgame && lowerPiece === 'k') {
+        squareTable = pieceSquareTables?.ke
       }
 
-      if (!squareTable) return 0;
+      if (!squareTable) return 0
 
       if (!isWhite) {
-        squareTable = [...squareTable].reverse();
+        squareTable = [...squareTable].reverse()
       }
 
-      const squareValues = mapArrayToChessboard(squareTable);
-      const piecePositionBonus = squareValues[pos];
+      const squareValues = mapArrayToChessboard(squareTable)
+      const piecePositionBonus = squareValues[pos]
 
-      return piecePositionBonus * (isWhite ? 1 : -1);
+      return piecePositionBonus * (isWhite ? 1 : -1)
     })
-    .reduce((total, current) => total + current, 0);
+    .reduce((total, current) => total + current, 0)
 
-export default calculatePiecePositionBonus;
+export default calculatePiecePositionBonus
