0d33398f87423d990e54b1598b95df6238017e88
Krzysztof Kalamarski <k.kalamarski@exlabs.pl>
2021-12-26 22:41:53 +0100

    ⚡️ Removed server

--- engine/webWorker.ts
+++ engine/webWorker.ts
@@ -2,7 +2,9 @@ const findBestMove = async (
   FEN: string,
   depth: number
 ): Promise<[string, string, number]> => {
-  const worker = new Worker("./index.ts");
+  const worker = new Worker(new URL("./index.ts", import.meta.url), {
+    type: "module",
+  });
 
   const bestMove = await new Promise<[string, string, number]>((resolve) => {
     worker.addEventListener("message", (message: any) => {
