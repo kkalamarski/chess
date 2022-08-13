ca70697fff22235d7d8d4cad6a6c2ed59b393a5c
Krzysztof Kalamarski <k.kalamarski@exlabs.pl>
2022-08-13 21:11:09 +0200

    encode fen

--- pages/api/move.ts
+++ pages/api/move.ts
@@ -3,7 +3,7 @@ import httpHandler from '../../utils/httpHandler'
 
 export default httpHandler({
   async get(req, res) {
-    const FEN: string = req.query.fen as string
+    const FEN: string = (new Buffer(req.query.fen as string, 'base64')).toString()
     const depth: number = Number(req.query.depth) || 2
     const bestMove = findBestMove(FEN, depth)
 
